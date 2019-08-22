8e7575e7170d1885d89de0583409bc63262a39a3
lqj679ssn <lqj679ssn@qq.com>
2019-08-22 13:14:31 +0800

    修复手机端无法显示的错误

--- js/neword.js
+++ js/neword.js
@@ -1,22 +1,4 @@
 class Neword {
-    static CharTemplate = template`
-    <div class="cross-box">
-        <div class="square left top"></div>
-        <div class="square left bottom"></div>
-        <div class="square right top"></div>
-        <div class="square right bottom"></div>
-        <div class="sub-box">
-            <div class="skew-box top-left"></div>
-        </div>
-        <div class="sub-box">
-            <div class="skew-box bottom-right"></div>
-        </div>
-        <div class="sub-box">
-            <div class="text">${0}</div>
-        </div>
-    </div>
-    `;
-
     constructor({titleBox, pinyinBox, contentTitle, contentDesc, inputBox}) {
         this.titleBox = document.getElementById(titleBox);
         this.pinyinBox = document.getElementById(pinyinBox);
@@ -33,9 +15,25 @@ class Neword {
     }
 
     setText(text) {
+        let charTemplate = template`
+            <div class="cross-box">
+                <div class="square left top"></div>
+                <div class="square left bottom"></div>
+                <div class="square right top"></div>
+                <div class="square right bottom"></div>
+                <div class="sub-box">
+                    <div class="skew-box top-left"></div>
+                </div>
+                <div class="sub-box">
+                    <div class="skew-box bottom-right"></div>
+                </div>
+                <div class="sub-box">
+                    <div class="text">${0}</div>
+                </div>
+            </div>`;
         this.titleBox.innerHTML = '';
         for (let c of text) {
-            let html = stringToHtml(Neword.CharTemplate(c));
+            let html = stringToHtml(charTemplate(c));
             this.titleBox.appendChild(html);
         }
         Request.post('https://tools.6-79.cn/v1/language/character-to-pinyin', {text: text})
